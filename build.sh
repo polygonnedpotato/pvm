6429bf4c63c8844c6c0daa8e292f3bf50cd93c9d
elburg the polygonned potato <73083772+polygonnedpotato@users.noreply.github.com>
2021-06-25 03:56:20 +0000

    hey i did stuff

---
+++ build.sh
@@ -0,0 +1,3 @@
+rm ./main
+clang++-7 -pthread -std=c++17 -o main main.cpp
+./main
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
-int main() {
-  std::cout << "WARNING! PVM IS NOT READY FOR PUBLIC USAGE YET!\n";
+std::string name{"PowerVM"};std::string version{"0.0.0.0"};
+std::string fms{"WARNING! "+name+" IS NOT READY FOR PUBLIC USAGE YET!\nUSE OTHER SOFTWARE SUCH AS QEMU, KVM, VBOX, OR OTHERS."};
+std::string thead(){
+  return name+" "+version;
+}
+int main(){
+  std::cout<<thead()<<"\n";
+  std::cout<<fms<<"\n";
 }
